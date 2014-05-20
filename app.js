c014c34b9bff991916c19e146997c6e6e750f740
inspirAZN <jcaluza@ucsd.edu>
2014-05-20 02:30:38 -0700

    Inintial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+// route includes
+
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+//app.engine('html', require('hogan-express'));
+//app.set('view engine', 'html');
+app.set('view engine', 'mustache');
+app.engine('mustache', require('hogan-middleware').__express);
+
+
+//app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// routes
+app.get('/', routes.index);
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
