a810c1bdc4115d6b6254fb1450b1f08d6ac5c6d8
cristiadu <cristiadu@gmail.com>
2014-05-24 05:49:34 -0700

    Adding authentication functions

--- app.js
+++ app.js
@@ -4,9 +4,20 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+/* Routes variables for files*/
+var indexRoute = require('./routes/index');
+var userRoute = require('./routes/user');
+/* END Routes variables for files */
+
+//load environment variables
+var dotenv = require('dotenv');
+dotenv.load();
 
 // route includes
 
@@ -28,6 +39,14 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+
+/* Passport Config Use*/
+app.use(express.cookieParser('secret'));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+/*END Passport Config Use*/
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,11 +55,35 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// routes
-app.get('/', routes.index);
+/* Passport Config General */
+var User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+/* END Passport Config General */
 
+// Mongoose Connect DB
+mongoose.connect(process.env.MONGOHQ_URL);
 
+/* Routes */
+
+//GET Requests
+app.get('/',ensureAuthenticated,indexRoute.index);
+app.get('/logout',ensureAuthenticated,userRoute.logout);
+app.get('/signup',userRoute.signup);
+
+// POST requests
+app.post('/login',userRoute.login);
+app.post('/confirmSignup',userRoute.saveUser);
+
+/* END Routes */
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// Middleware to ensure if the user is authenticated
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.render('login');
+}
---
+++ routes/user.js
@@ -0,0 +1,32 @@
+var User = require('../models/user');
+var passport = require('passport');
+
+exports.login =  function(req,res){
+passport.authenticate('local',{failureRedirect: '/' })(req,res,function() {
+      res.redirect('/');
+  });
+}
+
+
+exports.logout = function(req, res) {
+	req.logout();
+    res.redirect('/');
+}
+
+exports.signup = function(req, res) {
+	res.render('signup', { title: 'Activity Viz'});
+}
+
+exports.saveUser = function(req, res) {
+	User.register(new User({ username : req.body.username, name: req.body.name }), req.body.password, function(err, user) {
+        if (err) {
+        	console.log(err);
+            return res.render('xx', { user : user });
+        }
+ 
+        passport.authenticate('local',{failureRedirect: '/' })(req, res, function () {
+          res.redirect('/');
+        });
+    });
+}
+
